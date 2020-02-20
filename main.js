600a8bb85a6eddc7e4aeb4f05c8392b93e6b32aa
Gunnar Neuendorf <gunnar.neuendorf@gmail.com>
2020-02-20 15:24:59 +0100

    test request

--- main.js
+++ main.js
@@ -148,7 +148,7 @@ class BdgCalendar extends utils.Adapter {
 				json: true
 			},
 			function(error, response, content) {
-				this.log.info(content);
+				BdgCalendar.log.debug(content);
 				//console.info('request done');
 				//return response.body;
 				return content;
